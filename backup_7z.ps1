3a4d3689787b899b3d6acc1bf68e4a4c1b79cfde
Ridvik Pal <ridvikpal@gmail.com>
2024-12-22 15:38:54 -0500

    Added better print statements to inform the user of what's happening.

--- backup_7z.ps1
+++ backup_7z.ps1
@@ -9,8 +9,16 @@ if (!(Test-Path -Path $sevenZipPath)) {
 # Today's date to archive the backup
 $date = (Get-Date).ToString('MM-dd-yyyy')
 
+# The local backup directory path
+$localBackupDirectory = "$env:USERPROFILE\Backup"
+
 # Define the output archive path and name
-$archiveName = "$env:USERPROFILE\Backup\$date-backup.7z"  # Adjust output path and name as necessary
+$archiveName = "$localBackupDirectory\$date-backup.7z"  # Adjust output path and name as necessary
+
+# Check if the backup folder exists and if not, then create it
+if (!(Test-Path -PathType Container -Path $localBackupDirectory)) {
+    New-Item -ItemType Directory -Path $localBackupDirectory
+}
 
 # Define the folders to include in the archive
 $foldersToArchive = @(
@@ -22,6 +30,13 @@ $foldersToArchive = @(
     "$env:USERPROFILE\Downloads"
 )
 
+# Inform the user which folders are being backed up
+Write-Host "Backing up the following folders:`n"
+foreach ($folder in $foldersToArchive) {
+    Write-Host $folder
+}
+Write-Host "`nBackup will be stored in the $archiveName archive locally`n"
+
 # Build the command to create the 7zip archive
 $cmdArgs = @(
     "a",                    # 'a' means to add files to the archive
@@ -29,16 +44,13 @@ $cmdArgs = @(
     $archiveName            # Output archive file name
     ) + $foldersToArchive   # Add the folders to include
 
-Write-Host "Backing up the following folders: $foldersToArchive"
-
-Write-Host "Backup will be stored in the $archiveName archive locally"
-
 # Run the 7z command with the specified arguments
 Start-Process -FilePath $sevenZipPath -ArgumentList $cmdArgs -NoNewWindow -Wait
 
 # Ask the user for the external drive they want to backup the archive ot
 $driveLetter = (Read-Host -Prompt "Please enter the external drive letter to backup archive to (make sure the drive is mounted)").ToUpper()
 
+# Check if the external drive is mounted at that location
 if (!(Test-Path -PathType Container -Path "${driveLetter}:")) {
     throw "Unable to detect the ${driveLetter}: drive. Please ensure it is properly mounted."
 }
@@ -46,13 +58,16 @@ if (!(Test-Path -PathType Container -Path "${driveLetter}:")) {
 # The backup directory on the external drive
 $backupDirectory = "${driveLetter}:\Backup"
 
-# Check if the backup folder exists and if not, then create it
+# Check if the backup folder exists on the external drive and if not, then create it
 if (!(Test-Path -PathType Container -Path $backupDirectory)) {
     New-Item -ItemType Directory -Path $backupDirectory
 }
 
 # Copy the archive to the external drive
 Copy-Item $archiveName -Destination "$backupDirectory\" 
+
+# Inform the user the backup archive was copied to the external drive
+Write-Host "`nThe backup archive was copied to $backupDirectory\`n"
 
 # Wait for the user to click enter to exit
 Read-Host -Prompt "Backup completed. Press enter to exit"
