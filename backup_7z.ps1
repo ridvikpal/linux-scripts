cba9df83c82f8bddcd2a64ccbe99632974e282b4
Ridvik Pal <ridvikpal@gmail.com>
2024-12-22 14:53:39 -0500

    Added some error checking to the 7z backup script.

--- backup_7z.ps1
+++ backup_7z.ps1
@@ -1,5 +1,10 @@
 ﻿# Define the path to 7z executable
-$sevenZipPath = "C:\Program Files\7-Zip\7z.exe"  # Adjust this path if necessary
+$sevenZipPath = "C:\Program Files\7-Zip\7z.exe"
+
+# Check if 7zip is installed
+if (!(Test-Path -PathType Container $sevenZipPath)) {
+    throw "The 7zip executable was not found at $sevenZipPath. Please check your 7zip installation."
+}
 
 # Today's date to archive the backup
 $date = (Get-Date).ToString('MM-dd-yyyy')
@@ -14,6 +19,7 @@ $foldersToArchive = @(
     "$env:USERPROFILE\Music", 
     "$env:USERPROFILE\Videos", 
     "$env:USERPROFILE\Pictures"
+    "$env:USERPROFILE\Downloads"
 )
 
 # Build the command to create the 7zip archive
@@ -23,11 +29,19 @@ $cmdArgs = @(
     $archiveName            # Output archive file name
     ) + $foldersToArchive   # Add the folders to include
 
+Write-Host "Backing up the following folders: $foldersToArchive"
+
+Write-Host "Backup will be stored in the $archiveName archive locally"
+
 # Run the 7z command with the specified arguments
 Start-Process -FilePath $sevenZipPath -ArgumentList $cmdArgs -NoNewWindow -Wait
 
 # Ask the user for the external drive they want to backup the archive ot
-$driveLetter = (Read-Host -Prompt "External drive letter to backup archive to (make sure the drive is mounted)").ToUpper()
+$driveLetter = (Read-Host -Prompt "Please enter the external drive letter to backup archive to (make sure the drive is mounted)").ToUpper()
+
+if (!(Test-Path -Path "${driveLetter}:\")) {
+    throw "Unable to detect the ${driveLetter}: drive. Please ensure it is properly mounted."
+}
 
 # The backup directory on the external drive
 $backupDirectory = "${driveLetter}:\Backup"
@@ -41,4 +55,4 @@ if (!(Test-Path -PathType Container $backupDirectory)) {
 Copy-Item $archiveName -Destination "$backupDirectory\" 
 
 # Wait for the user to click enter to exit
-Read-Host -Prompt "Press enter to exit"
+Read-Host -Prompt "Backup completed. Press enter to exit"
