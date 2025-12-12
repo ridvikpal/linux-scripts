f56adf369810b8634e7b4b76dd6c316775f13744
Ridvik Pal <ridvikpal@gmail.com>
2025-12-12 09:59:44 -0500

    Updated comments for keep awake script.

--- keep_awake.sh
+++ keep_awake.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# Script to keep the laptop awake using systemd-inhibit and sleep
+# Script to keep the laptop awake using gnome-session-inhibit
 
 # Setup the reason for the inhibition.
-# It will be visible in 'systemd-inhibit --list'.
+# It will be visible in 'gnome-session-inhibit -l'.
 INHIBIT_REASON="Running keep_awake.sh script"
 
 # Inform the user the system is being inhibited
-echo "Inhibiting the GNOME desktop session from locking, blanking or suspending"
+echo "Inhibiting the GNOME desktop session from idling or suspending"
 echo "Reason: $INHIBIT_REASON"
 echo "Press [Ctrl+C] to stop the script."
 echo ""
 
-# The systemd-inhibit command creates a lock.
-# The 'sleep infinity' command keeps the script running indefinitely
-# until manually interrupted (e.g., with Ctrl+C).
-# The --why and --what flags provide context to the system.
-# --what=idle: Prevents the screen from dimming or the system from idling.
-# --what=sleep: Prevents system from going into suspend/hibernate.
-# --what=shutdown: Prevents system from shutting down/rebooting.
+# Use gnome-session-inhibit to prevent the desktop session from idling or suspending
 gnome-session-inhibit \
     --reason "$INHIBIT_REASON" \
     --inhibit suspend:idle \
