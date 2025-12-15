bc08ef9ebabfd91eba75e28e2056e19403d76f84
Ridvik Pal <ridvikpal@gmail.com>
2025-12-15 09:37:49 -0500

    Added setup scripts to install and remove packages.

---
+++ setup/bash/install_apt_packages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# first ensure the user is running this script as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 
+   exit 1
+fi
+
+# then ensure the user has upgraded their machine first
+read -p "This script will install Ubuntu apt packages. Did you run apt update && apt upgrade first? (Y/n) " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # if the user did not, then exit
+    exit 1
+fi
+
+# install nonfree ubuntu extras such as MS fonts and codecs
+apt-get install -y ubuntu-restricted-extras
+
+# install the default java runtime
+apt-get install -y default-jre
+
+# install git
+apt-get install -y git
+
+# install python3
+apt-get install -y python3 python3-pip python3-venv
+
+# install docker
+apt-get install -y docker.io docker-compose-v2
+
+# install latex via texlive
+apt-get install -y texlive texlive-latex-extra chktex
+
+# install libreoffice
+apt-get install -y libreoffice
+
+# install vlc
+apt-get install -y vlc
---
+++ setup/bash/install_snap_packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# first ensure the user is running this script as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 
+   exit 1
+fi
+
+# then ensure the user has upgraded their machine first
+read -p "This script will install Ubuntu snap packages. Did you run snap refresh first? (Y/n) " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # if the user did not, then exit
+    exit 1
+fi
+
+# install chromium and related chromium codecs
+snap install chromium chromium-ffmpeg
+
+# install spotify
+snap install spotify
+
+# install VS Code
+snap install code --classic
+
+# install Node JS
+snap install node --classic --channel=24
