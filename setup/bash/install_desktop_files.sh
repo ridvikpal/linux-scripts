cb3df9f8c11a83f26ef78e2883cb81307446fb3e
Ridvik Pal <ridvikpal@gmail.com>
2026-02-11 10:24:58 -0500

    Modified desktop files setup script to not add keep gnome awake script because I'm using XFCE now.

--- setup/bash/install_desktop_files.sh
+++ setup/bash/install_desktop_files.sh
@@ -9,10 +9,6 @@
 REPO_PATH="/home/ridvikpal/github/scripts"
 DESKTOP_PATH="/home/ridvikpal/Desktop"
 
-# install the desktop file for keep_gnome_awake.sh
-echo "Symlinking the .desktop file for keep_gnome_awake.sh"
-ln -sf "${REPO_PATH}/utilities/desktop/keep_gnome_awake.desktop" "${DESKTOP_PATH}/keep_gnome_awake.desktop"
-
 # install the desktop file for pc_to_ext_drive.sh
 echo "Symlinking the .desktop file for pc_to_ext_drive.sh"
 ln -sf "${REPO_PATH}/backup/desktop/pc_to_ext_drive.desktop" "${DESKTOP_PATH}/pc_to_ext_drive.desktop"
