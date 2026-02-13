e87e466090d1d124a7b966d4fcfdffafd5a2e47b
Ridvik Pal <ridvikpal@gmail.com>
2026-02-12 19:07:34 -0500

    Removed xsct related scripts and systemd files. Gammastep is better

--- setup/bash/setup_root_systemd_units.sh
+++ setup/bash/setup_root_systemd_units.sh
@@ -14,18 +14,10 @@ fi
 REPO_PATH="/home/ridvikpal/github/linux-scripts"
 SYSTEMD_PATH="/etc/systemd/system"
 
-# symlink the service file to the systemd system directory
-echo "Symlinking sync-monitor-gdm.service..."
-ln -sf "${REPO_PATH}/setup/systemd/sync-monitor-gdm.service" "${SYSTEMD_PATH}/sync-monitor-gdm.service"
-
-# symlink the path file to the systemd system directory
-echo "Symlinking sync-monitor-gdm.path..."
-ln -sf "${REPO_PATH}/setup/systemd/sync-monitor-gdm.path" "${SYSTEMD_PATH}/sync-monitor-gdm.path"
+# symlink or copy your systemd unit files here
 
 # reload systemd so it recognizes the new files
 echo "Reloading systemd..."
 systemctl daemon-reload
 
-# enable the new systemd path file
-echo "Enabling sync-monitor-gdm.path..."
-systemctl enable --now sync-monitor-gdm.path
+# enable your systemd unit files here
--- setup/bash/setup_user_systemd_units.sh
+++ setup/bash/setup_user_systemd_units.sh
@@ -11,18 +11,10 @@ SYSTEMD_PATH="/home/ridvikpal/.config/systemd/user"
 # Make the user systemd path if it doesn't exist already
 mkdir -p "${SYSTEMD_PATH}"
 
-# symlink the service file to the systemd system directory
-echo "Symlinking set-display-temperature.service..."
-ln -sf "${REPO_PATH}/setup/systemd/set-display-temperature.service" "${SYSTEMD_PATH}/set-display-temperature.service"
-
-# symlink the path file to the systemd system directory
-echo "Symlinking set-display-temperature.timer"
-ln -sf "${REPO_PATH}/setup/systemd/set-display-temperature.timer" "${SYSTEMD_PATH}/set-display-temperature.timer"
+# symlink or copy your systemd unit files here
 
 # reload systemd so it recognizes the new files
 echo "Reloading systemd..."
 systemctl --user daemon-reload
 
-# enable the new systemd path file
-echo "Enabling set-display-temperature.timer..."
-systemctl --user enable --now set-display-temperature.timer
+# enable your systemd unit files here
