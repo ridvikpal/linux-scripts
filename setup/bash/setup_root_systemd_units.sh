f07986394b3149a9b9f0e22b00d72a2b8a1a5976
Ridvik Pal <ridvikpal@gmail.com>
2026-02-15 15:18:25 -0500

    Minor comment change

--- setup/bash/setup_root_systemd_units.sh
+++ setup/bash/setup_root_systemd_units.sh
@@ -14,11 +14,11 @@ fi
 REPO_PATH="/home/ridvikpal/github/linux-scripts"
 SYSTEMD_PATH="/etc/systemd/system"
 
-# symlink the service file to the systemd system directory
+# symlink the sync-monitor-gdm service file to the systemd system directory
 echo "Symlinking sync-monitor-gdm.service..."
 ln -sf "${REPO_PATH}/setup/systemd/sync-monitor-gdm.service" "${SYSTEMD_PATH}/sync-monitor-gdm.service"
 
-# symlink the path file to the systemd system directory
+# symlink the sync-monitor-gdm path file to the systemd system directory
 echo "Symlinking sync-monitor-gdm.path..."
 ln -sf "${REPO_PATH}/setup/systemd/sync-monitor-gdm.path" "${SYSTEMD_PATH}/sync-monitor-gdm.path"
 
@@ -26,6 +26,6 @@ ln -sf "${REPO_PATH}/setup/systemd/sync-monitor-gdm.path" "${SYSTEMD_PATH}/sync-
 echo "Reloading systemd..."
 systemctl daemon-reload
 
-# enable the new systemd path file
+# enable the new sync-monitor-gdm path file
 echo "Enabling sync-monitor-gdm.path..."
 systemctl enable --now sync-monitor-gdm.path
