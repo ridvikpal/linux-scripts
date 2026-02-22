b883a630581c1e9746a485b543ad28c91b15bd64
Ridvik Pal <ridvikpal@gmail.com>
2026-02-22 15:09:39 -0500

    Updated sync monitor gdm service to work with Ubuntu

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 These are a collection of bash scripts I created to automate things such as backing up my files, installing programs and keeping my laptop awake when needed. For some of these scripts I created .desktop files so I can quickly run them as desktop apps.
 
-Please note that I use Debian 13, so some commands may not be consistent across other *nix flavours and distributions. For example, the hardcoded paths like `/home/ridvikpal` will obviously need to be changed for your username. Feel free to copy these and modify them to your needs!
+Please note that I use Ubuntu 24, so some commands may not be consistent across other *nix flavours and distributions. Also, the hardcoded paths like `/home/ridvikpal` will obviously need to be changed for your username. Feel free to copy these and modify them to your needs!
--- setup/bash/sync_monitor_gdm.sh
+++ setup/bash/sync_monitor_gdm.sh
@@ -2,7 +2,7 @@
 
 ####################
 # This script copies your GNOME Shell monitor configuration to
-# the gdm configuration folder on Debian
+# the gdm configuration folder on Ubuntu
 # so that both monitor configurations (e.g., scaling) are synced
 #
 # If you're using another Linux distro, you can find your gdm config folder
@@ -20,17 +20,17 @@ fi
 
 # define reusable paths
 GNOME_SHELL_CONFIG_PATH="/home/ridvikpal/.config"
-GDM_CONFIG_PATH="/var/lib/gdm3/.config"
+GDM_CONFIG_PATH="/var/lib/gdm3/seat0/config"
 
 # define the GDM user, which can be found via grep gdm /etc/passwd
-GDM_USER="Debian-gdm"
+GDM_USER="gdm"
 
 # Inform the user what the script is doing
-echo "Copying GNOME Shell monitor configuration to GDM configuration"
+echo "Copying GNOME Shell monitor configuration to GDM configuration..."
 
 # then copy the monitors.xml file to the gdm configuration directory
 cp "${GNOME_SHELL_CONFIG_PATH}/monitors.xml" "${GDM_CONFIG_PATH}/"
 
 # Update the permissions to ensure the copied monitors file is
-# owned by the Debian-gdm user
+# owned by the gdm user
 chown "${GDM_USER}:${GDM_USER}" "${GDM_CONFIG_PATH}/monitors.xml"
